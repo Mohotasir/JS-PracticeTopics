0f0ccb815c7f6af89f9439d0295271c49733d8ab
mohotasir <talukdertahsin643@gmail.com>
2023-12-14 22:13:58 +0600

    first commit

---
+++ AsyncAwait js/main.js
@@ -0,0 +1,57 @@
+
+ const isPay =  true;
+ const mark = 90;
+function enroll(){
+   console.log("course processing..");
+    const promise =  new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            if(isPay)
+              resolve();
+            else
+               reject("paymet fail");
+          }, 1000);
+    });
+
+    return promise;
+}
+
+function progress(){
+    const promise =  new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            if(mark>=80)
+              resolve();
+            else
+               reject("could not get enough mark");
+          }, 1000);
+    });
+   return promise;
+}
+
+function getCertificate(){
+   
+    const promise =  new Promise((resolve)=>{
+        setTimeout(() => {
+            
+             resolve("congo...");
+            
+          }, 1000);
+    });
+
+    return promise;
+
+}
+
+async function course(){
+  try{
+    await enroll();
+    await progress();
+    const msg =  await getCertificate();
+    console.log(msg);
+    
+  }catch(err){
+    console.log(err);
+  }
+}
+
+
+course();
